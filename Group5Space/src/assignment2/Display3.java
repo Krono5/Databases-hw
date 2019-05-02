8a7489ac53fc7edf8b1e9f290c0a624c6412fead
Josh <33630130+jw4302@users.noreply.github.com>
2019-05-01 23:05:01 -0400

    Display3 v4.00050
    Put the class file in the right place this time.
    Button actions and DataTable Values

--- Group5Space/src/assignment2/Display3.java
+++ Group5Space/src/assignment2/Display3.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 
 import javax.swing.*;
 
@@ -15,68 +16,39 @@ import javax.swing.*;
 public class Display3 {
 
 	
-
+ 
 	private JButton textButton, imageButton;
 	private static JLabel textLabel, imageLabel;
-	
 	private JFrame displayFrame;
 	private JPanel  statsFrame, buyFrame;
 	
 
 	public static String username;
 	
-
+	static JTable dataTable;
+	static JScrollPane header;
 
 	public Display3 () throws Exception {
 		
 		
 		displayFrame = new JFrame("Display 3");
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		
+		JPanel southPanel = new JPanel();
+		header = new JScrollPane();
 		displayFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		displayFrame.setLocationRelativeTo(null);
-		JScrollPane radioScrollPane, buttonScrollPane;
-		radioScrollPane = new JScrollPane();
-		buttonScrollPane= new JScrollPane();
+		refreshValues();
 		
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, radioScrollPane, buttonScrollPane);
-		Dimension minimumSize = new Dimension(400, 400);
-		splitPane.setOneTouchExpandable(true);
-		splitPane.setDividerLocation(400);
-		radioScrollPane.setMinimumSize(minimumSize);
-		buttonScrollPane.setMinimumSize(minimumSize);
-		radioScrollPane.add(radioButtons());
-		//mainPanel.add("Center",dataTable());
-		mainPanel.add("Center",splitPane);
-		//mainPanel.add("South",radioButtons());
+		//JLabel userName = new JLabel(getUserName());
 		
-		/*
-		 * mainPanel.setSize(800,800); mainPanel.setLayout(new BorderLayout());
-		 * 
-		 * 
-		 * statsFrame = new JPanel(); statsFrame.setSize(400,400); buyFrame = new
-		 * JPanel(); buyFrame.setSize(400,400);
-		 * 
-		 * JScrollPane radioScrollPane = new JScrollPane(); JScrollPane
-		 * buttonScrollPane= new JScrollPane(); splitPane = new
-		 * JSplitPane(JSplitPane.HORIZONTAL_SPLIT, radioScrollPane, buttonScrollPane);
-		 * Dimension minimumSize = new Dimension(100, 50);
-		 * radioScrollPane.setMinimumSize(minimumSize);
-		 * buttonScrollPane.setMinimumSize(minimumSize);
-		 * 
-		 * radioScrollPane.add(radioButtons());
-		 * 
-		 * 
-		 * buttonScrollPane.add(buyShipsButton());
-		 * buttonScrollPane.add(buyCargoButton());
-		 * buttonScrollPane.add(buyCrusiersButton());
-		 * buttonScrollPane.add(buyBaublesButton());
-		 * 
-		 * statsFrame.add(dataTable()); buyFrame.add(splitPane);
-		 * mainPanel.add(statsFrame); mainPanel.add(buyFrame);
-		 */
-	    
+		//mainPanel.add("North", userName);
+		mainPanel.add("Center",header);
+		mainPanel.add("South",southPanel);
+		southPanel.add(buyShipsButton());
+		southPanel.add(buyCargoButton());
+		southPanel.add(buyCrusiersButton());
+		southPanel.add(buyBaublesButton());
 	    displayFrame.add(mainPanel);
 	    displayFrame.pack();
 		displayFrame.setVisible(true);
@@ -98,14 +70,28 @@ public class Display3 {
 		  return username;
 	}
 	
-	private JTable dataTable() {
-		String [] columnNames = {"Resources12", "Factories21", "Bauble12s", "Shipyards12", "Cargo Ships12", "Crusier Ships12"};
-		//write out the call to get all the info to fill the JTable
-		//Object [][] data = { getResources(), getFactories(), getBaubles(), GetShipyards(), getCargoShips(), getCrusierShips()};
-		Object [][] data = {{"Resources", "Factories", "Baubles", "Shipyards", "Cargo Ships", "Crusier Ships"},
-				{"Resources", "Factories", "Baubles", "Shipyards", "Cargo Ships", "Crusier Ships"} };
-		//Creating dataTable to show off how many of what a player and planet has
+	private static JTable dataTable( int resources , int factories , int baubles , int shipyards , int cargos , int cruisers ) {
+		Vector<String> columnNames = new Vector<String>();
+		 columnNames.addElement("Resources");
+		 columnNames.addElement("Factories");
+		 columnNames.addElement("Baubles");
+		 columnNames.addElement("Shipyards");
+		 columnNames.addElement("Cargo Ships");
+		 columnNames.addElement("Crusier Ships");
+		 
+		 Vector<Vector> data = new Vector<Vector>();
+		 Vector<Integer> row = new Vector<Integer>();
+		 row.addElement( resources );
+		 row.addElement(factories);
+		 row.addElement(baubles);
+		 row.addElement(shipyards);
+		 row.addElement(cargos);
+		 row.addElement(cruisers);
+		 data.addElement( row );
+		
+		
 		JTable dataTable = new JTable(data, columnNames);
+		
 		dataTable.setPreferredScrollableViewportSize(new Dimension(150, 70));
         dataTable.setFillsViewportHeight(false);
         dataTable.setVisible(true);
@@ -113,71 +99,58 @@ public class Display3 {
         return dataTable;
 	}
 
+	public static void refreshValues() {
+		int resourceValue = -1;
+		int factoriesValue = -1;
+		int baublesValue = -1;
+		int shipyardValue = -1;
+		int cargoValue = -1;
+		int crusierValue = -1;
+		Statement stmt;
+		try {
+			stmt = SpaceController.dbConnection.createStatement();
+			String getValues= new String("CALL csc371_02.getValues();"); // StoredProcedure to retrieve player Names
+			ResultSet rs = stmt.executeQuery(getValues);
+			while (rs.next()) {
+				resourceValue = rs.getInt(1);
+				factoriesValue= rs.getInt(2);
+				baublesValue= rs.getInt(3);
+				shipyardValue= rs.getInt(4);
+				cargoValue= rs.getInt(5);
+				crusierValue= rs.getInt(6);
+				
+			}
+			dataTable = dataTable( resourceValue , factoriesValue, baublesValue, shipyardValue, cargoValue, crusierValue);
+			header.setViewportView( dataTable );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	
-	/*
-	 * being able to pick which things to buy 
-	 */
-	private JPanel radioButtons() {
-		String shipStrng = "Ships";
-		String cargoStrng = "Cargo Ships";
-		String crusierStrng = "Crusier Ships";
-		String baublesStrng = "Baubles";
-		
-		JRadioButton shipButton = new JRadioButton(shipStrng);
-		shipButton.setMnemonic(KeyEvent.VK_S);
-	    shipButton.setActionCommand(shipStrng);
-	    shipButton.setSelected(true);
-		
-		JRadioButton cargoButton = new JRadioButton(cargoStrng);
-		cargoButton.setMnemonic(KeyEvent.VK_C);
-	    cargoButton.setActionCommand(cargoStrng);
-	    cargoButton.setSelected(true);
-    
-		JRadioButton cruiserButton = new JRadioButton(crusierStrng);
-		cruiserButton.setMnemonic(KeyEvent.VK_R);
-	    cruiserButton.setActionCommand(crusierStrng);
-	    cruiserButton.setSelected(true);
-	    
-		JRadioButton baublesButton = new JRadioButton(baublesStrng);
-		baublesButton.setMnemonic(KeyEvent.VK_B);
-	    baublesButton.setActionCommand(baublesStrng);
-	    baublesButton.setSelected(true);
-		
-	    
-	    ButtonGroup group = new ButtonGroup();
-	    group.add(shipButton);
-	    group.add(cargoButton);
-	    group.add(cruiserButton);
-	    group.add(baublesButton);
-	 
-	    //Put the radio buttons in a column in a panel.
-        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
-        radioPanel.add(shipButton);
-        radioPanel.add(cargoButton);
-        radioPanel.add(cruiserButton);
-        radioPanel.add(baublesButton);
-     
-
-       
-        
-	    return radioPanel;
-		
-		
 	}
-	
 	/*
 	 *buy and update buttons for the split window 
 	 */
 	public static JButton buyShipsButton() {
 		JButton buyShips = new JButton("Buy a Ship");
-		/*
-		 * buyShips.addActionListener(new ActionListener() {
-		 * 
-		 * @Override public void actionPerformed(ActionEvent e) { selectedName =
-		 * list.getSelectedValue(); sendMessage(selectedName);
-		 * 
-		 * }
-		 */
+		
+		  buyShips.addActionListener(new ActionListener() {
+		  
+		  @Override public void actionPerformed(ActionEvent e) { 
+			  Statement stmt;
+			  try {
+			  stmt = SpaceController.dbConnection.createStatement();
+				String buyCargo = new String("CALL csc371_02.buyCargo();"); // StoredProcedure to retrieve player Names
+				stmt.execute(buyCargo);
+			  refreshValues();
+		  	} catch (SQLException el){
+		  		el.printStackTrace();
+		  	}
+		  }
+		  });
+		 
 		return buyShips;
 		
 	}
@@ -188,13 +161,13 @@ public class Display3 {
 	public static JButton  buyCargoButton() {
 		JButton buyCargoShips = new JButton("Buy a Cargo Ship");
 		
-		/*
-		 * buyCargoShips.addActionListener(new ActionListener() {
-		 * 
-		 * @Override public void actionPerformed(ActionEvent e) { selectedName =
-		 * list.getSelectedValue(); sendMessage(selectedName);
-		 * 
-		 * }
+		
+/*		  buyCargoShips.addActionListener(new ActionListener() {
+		  
+		  @Override public void actionPerformed(ActionEvent e) { selectedName =
+		  list.getSelectedValue(); sendMessage(selectedName);
+		  
+		  }
 		 */
 		return buyCargoShips;
 		
@@ -206,14 +179,14 @@ public class Display3 {
 	public static JButton buyCrusiersButton() {
 		JButton buyCrusiers = new JButton("Buy a Cruiser Ship");
 		
-		/*
-		 * buyCrusiers.addActionListener(new ActionListener() {
-		 * 
-		 * @Override public void actionPerformed(ActionEvent e) { selectedName =
-		 * list.getSelectedValue(); sendMessage(selectedName);
-		 * 
-		 * }
-		 */
+		
+/*		  buyCrusiers.addActionListener(new ActionListener() {
+		  
+		  @Override public void actionPerformed(ActionEvent e) { selectedName =
+		  list.getSelectedValue(); sendMessage(selectedName);
+		  
+		  }*/
+		 
 		return buyCrusiers;
 	}
 	
@@ -222,15 +195,17 @@ public class Display3 {
 	 */
 	public static JButton buyBaublesButton() {
 		JButton buyBaubles = new JButton("Buy more Baubles");
-		/*
-		 * buyBaubles.addActionListener(new ActionListener() {
-		 * 
-		 * @Override public void actionPerformed(ActionEvent e) { selectedName =
-		 * list.getSelectedValue(); sendMessage(selectedName);
-		 * 
-		 * }
+		
+/*		  buyBaubles.addActionListener(new ActionListener() {
+	  
+		  @Override public void actionPerformed(ActionEvent e) { selectedName =
+		  list.getSelectedValue(); sendMessage(selectedName);
+		  
+		  }
 		 */
 		return buyBaubles;
 	}
+		  
+	}
 
-}
+
